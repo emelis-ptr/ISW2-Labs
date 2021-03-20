603ea9f06c0e2b8e04d7bed40f4240ddb39a8a28
emelis-ptr <melipetrolo@gmail.com>
2021-03-20 11:59:19 +0100

    Implementazione del primo laboratorio.

---
+++ src/lab_one/Main.java
@@ -0,0 +1,59 @@
+package lab_one;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import utils.InitRepo;
+import utils.LogFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            retrieveCommentTicket();
+        } catch (NoHeadException | IOException e) {
+            LogFile.errorLog("Errore!");
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Metodo che verifica se esiste una determinata string nel commento di un commit
+     * @throws NoHeadException
+     * @throws IOException
+     */
+    public static void retrieveCommentTicket() throws NoHeadException, IOException {
+        Repository repo = InitRepo.repository();
+
+        Git git = new Git(repo);
+
+        Iterable<RevCommit> logs = git.log().call();
+
+        Iterator<RevCommit> iterator = logs.iterator();
+
+        ArrayList<RevCommit> list = new ArrayList<>();
+        String comment = "Added";
+
+        while (iterator.hasNext()) {
+
+            RevCommit rev = iterator.next();
+
+            if (rev.getFullMessage().contains(comment)) {
+                list.add(rev);
+            }
+        }
+
+        if (list.isEmpty()) {
+            LogFile.warnLog("Lista vuota");
+        } else {
+            LogFile.infoLog(list.toString());
+        }
+    }
+}
